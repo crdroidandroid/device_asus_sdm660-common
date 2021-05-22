1502b5528a233130dcc6c6c9ad8dba9efc815adf
ElectroPerf <kunmun.devroms@gmail.com>
2021-05-22 16:52:26 +0000

    Switch to Eva

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,8 +6,6 @@ git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8998_media.git
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git  prebuilts/gcc/linux-x86/aarch64/arm64-gcc
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git  prebuilts/gcc/linux-x86/aarch64/arm32-gcc
 rm -rf packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-18.1 packages/resources/devicesettings
 
@@ -20,6 +18,10 @@ git clone https://github.com/SonalSingh18/android_packages_apps_ZenParts.git pac
 
 # Shady Launcher
 git clone https://github.com/dhimanparas20/vendor_shadylauncher.git vendor/shadylauncher
+
+# GCC
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
 
 # Cache
 export USE_CCACHE=1 && ccache -M 50G && export CONFIG_STATE_NOTIFIER=y
